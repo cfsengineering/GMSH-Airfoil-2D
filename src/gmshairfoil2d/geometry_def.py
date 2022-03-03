4a559044bdd5d9a40f2de8fb04724f9860c3050f
Tony Govoni <tony.govoni@epfl.ch>
2022-03-03 16:35:43 +0100

    Change class input from tag to class

--- src/gmshairfoil2d/geometry_def.py
+++ src/gmshairfoil2d/geometry_def.py
@@ -11,59 +11,52 @@ import numpy as np
 
 # This script contain the definition of geometrical objects needed to build
 # the geometry and latter the mesh
+
+
 class Point:
-    def __init__(self, x, y, z, meshSize):
+    def __init__(self, x, y, z, mesh_size):
         # position in space
         self.x = x
         self.y = y
         self.z = z
-        self.meshSize = meshSize
-        # point object are of dim 0
+        self.mesh_size = mesh_size
         self.dim = 0
         # create the gmsh object and store the tag of the geometric object
-        self.tag = gmsh.model.occ.addPoint(self.x, self.y, self.z, self.meshSize)
+        self.tag = gmsh.model.occ.addPoint(self.x, self.y, self.z, self.mesh_size)
 
 
 class Line:
-    def __init__(self, startTag, endTag):
+    def __init__(self, start_point, end_point):
         # tag of the two points forming the line
-        self.startTag = startTag
-        self.endTag = endTag
-        # line object are of dim 1
+        self.start_point = start_point
+        self.end_point = end_point
         self.dim = 1
         # create the gmsh object and store the tag of the geometric object
-        self.tag = gmsh.model.occ.addLine(self.startTag, self.endTag)
+        self.tag = gmsh.model.occ.addLine(self.start_point.tag, self.end_point.tag)
 
 
 class CurveLoop:
-    def __init__(self, startTag, endTag):
-        # tag of the starting and ending line
-        self.startTag = startTag
-        self.endTag = endTag
-        # number of line in the curve
-        self.nbLine = endTag - startTag + 1
-        # Assuming that the Line between the start and end tag form a closed loop
-        self.curveTags = np.linspace(self.startTag, self.endTag, self.nbLine).astype(
-            int
-        )
-        # CurveLoop object are of dim 1
-        self.dim = 1
+    def __init__(self, line_array):
+        # Line array must be in the order of the wanted CurveLoop and closed
+        self.line_array = line_array
+        self.tag_array = []
+        # generate the Lines tag array to folow
+        for line in line_array:
+            self.tag_array.append(line.tag)
         # create the gmsh object and store the tag of the geometric object
-        self.tag = gmsh.model.occ.addCurveLoop(self.curveTags)
+        self.tag = gmsh.model.occ.addCurveLoop(self.tag_array)
 
 
 class Circle:
-    def __init__(self, xc, yc, zc, r):
+    def __init__(self, xc, yc, zc, radius):
         # Position of the disk center
         self.xc = xc
         self.yc = yc
         self.zc = zc
-        # radius of the disk
-        self.r = r
-        # Disk object are of dim 1
+        self.radius = radius
         self.dim = 1
         # create the gmsh object and store the tag of the geometric object
-        self.tag = gmsh.model.occ.addCircle(self.xc, self.yc, self.zc, self.r)
+        self.tag = gmsh.model.occ.addCircle(self.xc, self.yc, self.zc, self.radius)
 
 
 class PlaneSurface:
@@ -73,15 +66,14 @@ class PlaneSurface:
     define holes in the surface domaine
     """
 
-    def __init__(self, curveloopTags):
-        if type(curveloopTags) == int:  # if only one curve loop is used
-            self.curveloopTags = [curveloopTags]  # put in a array
-        else:
-            self.curveloopTags = curveloopTags  # it is already an array
-        # PlaneSurface object are of dim 2
+    def __init__(self, curveLoop_array):
+        self.curveLoop_array = curveLoop_array
+        self.tag_array = []
+        for curveLoop in curveLoop_array:
+            self.tag_array.append(curveLoop.tag)
         self.dim = 2
         # create the gmsh object and store the tag of the geometric object
-        self.tag = gmsh.model.occ.addPlaneSurface(self.curveloopTags)
+        self.tag = gmsh.model.occ.addPlaneSurface(self.tag_array)
 
 
 # end
