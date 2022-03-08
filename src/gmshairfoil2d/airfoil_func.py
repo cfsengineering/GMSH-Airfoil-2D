2eb4e9274c5e064bc651a2654d7750a4cb3e094c
GovoniT <64786873+GovoniT@users.noreply.github.com>
2022-03-08 13:47:20 +0100

    Define main classes (#2)
    * Added a script containing the main geometric object
    
    * Change class input from tag to class
    
    * Minor change on the code format and remove of Temp.py in the main gitignore
    
    * Added the possibility to create a circle and rectangle with a defined mesh size
    
    * Replace a for loop with a list comprehension
    
    * Add Airfoil class
    
    * Added the possibility to construct the foil with a Spline
    
    * Add the auto creation of the database file + download the foil.dat only if not already downloaded
    
    * Add a .gitingore for the database
    
    * Change database file location
    
    * Add BC physical name for Airfoil, Rectangle box, and Circle farfield
    
    * Correction of the BC + add NACA 4 digit geom generator to airfoil2d
    
    * Add an example.py script and add the generated mesh.su2 to the gitignore
    
    * Rearrange functions
    
    * Add Doc String and separate the definition of the boundary condition from the init
    
    * correction on the example.py
    
    Co-authored-by: aidanjungo <aidan@cfse.ch>

---
+++ src/gmshairfoil2d/airfoil_func.py
@@ -0,0 +1,140 @@
+import requests
+import os
+import numpy as np
+
+
+def get_all_available_airfoil_names():
+    """
+    Request the airfoil list available at m-selig.ae.illinois.edu
+
+    Returns
+    -------
+    _ : list
+        return a list containing the same of the available airfoil
+    """
+    url = "https://m-selig.ae.illinois.edu/ads/coord_database.html"
+
+    r = requests.get(url)
+
+    airfoil_list = [t.split(".dat")[0] for t in r.text.split('href="coord/')[1:]]
+
+    print(f"{len(airfoil_list)} airfoils found:")
+    print(airfoil_list)
+
+    return airfoil_list
+
+
+def get_airfoil_file(airfoil_name):
+    """
+    Request the airfoil .dat file at m-selig.ae.illinois.edu and stores it (if found) in the 
+    database folder
+    
+    Parameters
+    ----------
+    airfoil_name : srt
+        name of the airfoil
+    """
+    path = "../../database"
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    url = f"https://m-selig.ae.illinois.edu/ads/coord/{airfoil_name}.dat"
+
+    r = requests.get(url)
+
+    if r.status_code != 200:
+        raise Exception(f"Could not get airfoil {airfoil_name}")
+
+    file = f"/{airfoil_name}.dat"
+
+    if not os.path.exists(path + file):
+        try:
+            open(path + file, "wb").write(r.content)
+        except OSError:
+            print("Failed creating the file :", file)
+
+
+def NACA_4_digit_geom(NACA_name, nb_points=100):
+    """
+    Compute the profile of a NACA 4 digits airfoil
+
+    Parameters
+    ----------
+    NACA_name : str
+        4 digit of the NACA airfoil
+    nb_points : int, optional
+            number of points for the disrcetisation of
+            the polar representation of the chord
+    Returns
+    -------
+    _ : int
+        return the 3d cloud of points representing the airfoil
+    """
+    theta_line = np.linspace(0, np.pi, nb_points)
+    x_line = 0.5 * (1 - np.cos(theta_line))
+
+    m = int(NACA_name[0]) / 100
+    p = int(NACA_name[1]) / 10
+    t = (int(NACA_name[2]) * 10 + int(NACA_name[3])) / 100
+    # thickness line
+    y_t = (
+        t
+        / 0.2
+        * (
+            0.2969 * x_line ** 0.5
+            - 0.126 * x_line
+            - 0.3516 * x_line ** 2
+            + 0.2843 * x_line ** 3
+            + -0.1036 * x_line ** 4
+        )
+    )
+
+    # cambered airfoil:
+    if p != 0:
+        # camber line front of the airfoil (befor p)
+        x_line_front = x_line[x_line < p]
+
+        # camber line back of the airfoil (after p)
+        x_line_back = x_line[x_line >= p]
+
+        # total camber line
+        y_c = np.concatenate(
+            (
+                (m / p ** 2) * (2 * p * x_line_front - x_line_front ** 2),
+                (m / (1 - p) ** 2)
+                * (1 - 2 * p + 2 * p * x_line_back - x_line_back ** 2),
+            ),
+            axis=0,
+        )
+        dyc_dx = np.concatenate(
+            (
+                (2 * m / p ** 2) * (p - x_line_front),
+                (2 * m / (1 - p) ** 2) * (p - x_line_back),
+            ),
+            axis=0,
+        )
+
+        theta = np.arctan(dyc_dx)
+        # upper and lower surface
+        x_u = x_line - y_t * np.sin(theta)
+        y_u = y_c + y_t * np.cos(theta)
+        x_l = x_line + y_t * np.sin(theta)
+        y_l = y_c - y_t * np.cos(theta)
+
+    # uncambered airfoil:
+    else:
+        y_c = 0 * x_line
+        dyc_dx = y_c
+        # upper and lower surface
+        x_u = x_line
+        y_u = y_t
+        x_l = x_line
+        y_l = -y_t
+
+    # concatenate the upper and lower
+    x = np.concatenate((x_u[:-1], np.flip(x_l[1:])), axis=0)
+    y = np.concatenate((y_u[:-1], np.flip(y_l[1:])), axis=0)
+
+    # create the 3d points cloud
+    return [[x[k], y[k], 0] for k in range(0, len(x))]
