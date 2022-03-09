b34d3d4e59ffd708211334dabc5e9e6a5dc33230
Tony Govoni <tony.govoni@epfl.ch>
2022-03-09 16:53:25 +0100

    Rework path to be multi OS compatible

--- tests/test_airfoi_func.py
+++ tests/test_airfoi_func.py
@@ -10,51 +10,50 @@ from gmshairfoil2d.airfoil_func import (
 
 
 LIB_DIR = os.path.dirname(gmshairfoil2d.__init__.__file__)
-database_dir = LIB_DIR + "/../database"
-test_data_dir = LIB_DIR + "/../tests/test_data"
+database_dir = os.path.join(LIB_DIR, os.path.dirname(LIB_DIR), "database")
+test_data_dir = os.path.join(LIB_DIR, os.path.dirname(LIB_DIR), "tests", "test_data")
 
 
 def test_get_all_available_airfoil_names():
     """
     Test if at least the database obtained containt some airfoils
-    
+
     """
 
     expected_airfoil = ["naca0010", "naca0018", "falcon", "goe510", "e1210"]
     current_airfoil_list = get_all_available_airfoil_names()
 
     for foil in expected_airfoil:
-        assert foil in current_airfoil_list, "test passed"
+        assert foil in current_airfoil_list
 
 
 def test_get_airfoil_file():
     """
-    Test if the download of some profiles is possible and if it is, check if 
+    Test if the download of some profiles is possible and if it is, check if
     they are conform
-    
+
     """
     profiles = ["naca0010", "naca4412"]
     # Remove airfoil if they exist
     for profile in profiles:
-        if os.path.exists(database_dir + "/" + profile + ".dat"):
-            os.remove(database_dir + "/" + profile + ".dat")
+        proflie_dl_path = os.path.join(database_dir, profile + ".dat")
+        if os.path.exists(proflie_dl_path):
+            os.remove(proflie_dl_path)
 
-    for profile in profiles:
+        proflie_test_path = os.path.join(test_data_dir, profile + ".dat")
         # Download them back
         get_airfoil_file(profile)
 
         # Test if download is correctly done
-        assert os.path.exists(database_dir + "/" + profile + ".dat")
+        assert os.path.exists(proflie_dl_path)
 
-        with open(database_dir + "/" + profile + ".dat", "r") as f:
-            dl_profil = f.read()
-        f.close()
+        with open(proflie_dl_path, "r") as f:
+            profil_dl = f.read()
 
-        with open(test_data_dir + "/" + profile + ".dat", "r") as f:
-            test_profil = f.read()
-        f.close()
+        with open(proflie_test_path, "r") as f:
+            profil_test = f.read()
         # Test if conform
-        assert test_profil == dl_profil
+        assert profil_test == profil_dl
 
 
 def test_NACA_4_digit_geom():
@@ -62,14 +61,15 @@ def test_NACA_4_digit_geom():
     Test if the NACA0012 profil and NACA4412 profil are correctly generated
     
     """
-    with open(test_data_dir + "/naca0012.txt", "rb") as f:
+    with open(os.path.join(test_data_dir, "naca0012.txt"), "rb") as f:
         naca0012 = pickle.load(f)
     f.close()
-    with open(test_data_dir + "/naca4412.txt", "rb") as f:
+    with open(os.path.join(test_data_dir, "naca4412.txt"), "rb") as f:
         naca4412 = pickle.load(f)
     f.close()
 
-    assert naca0012 == NACA_4_digit_geom("0012"), "test passed"
-    assert naca4412 == NACA_4_digit_geom("4412"), "test passed"
-    assert naca4412 != NACA_4_digit_geom("4413"), "test passed"
+    assert naca0012 == NACA_4_digit_geom("0012")
+    assert naca4412 == NACA_4_digit_geom("4412")
+    assert naca4412 != NACA_4_digit_geom("4413")
+    f.close()
     f.close()
--- tests/test_geometry_def.py
+++ tests/test_geometry_def.py
@@ -5,14 +5,14 @@ import gmshairfoil2d.__init__
 from gmshairfoil2d.geometry_def import Rectangle, Circle, PlaneSurface
 
 LIB_DIR = os.path.dirname(gmshairfoil2d.__init__.__file__)
-test_data_dir = LIB_DIR + "/../tests/test_data"
+test_data_dir = os.path.join(LIB_DIR, os.path.dirname(LIB_DIR), "tests", "test_data")
 
 
 def test_mesh_circle_into_rectangle():
     """
     Test if a simple generated mesh formed of a square domain with a circular
     hole inside is meshed correctly
-    
+
     """
     # Generate Geometry :
     gmsh.initialize()
@@ -24,14 +24,14 @@ def test_mesh_circle_into_rectangle():
     gmsh.model.occ.synchronize()
     # Generate mesh
     gmsh.model.mesh.generate(2)
-    gmsh.write(test_data_dir + "/mesh_test.su2")
+    gmsh.write(os.path.join(test_data_dir, "mesh_test.su2"))
     gmsh.finalize()
 
-    with open(test_data_dir + "/mesh_test.su2", "r") as f:
+    with open(os.path.join(test_data_dir, "mesh_test.su2"), "r") as f:
         mesh_test = f.read()
     f.close()
 
-    with open(test_data_dir + "/mesh.su2", "r") as f:
+    with open(os.path.join(test_data_dir, "mesh.su2"), "r") as f:
         mesh_origin = f.read()
     f.close()
     # Test if conform
