45f88592428b975e96592476534a7a4e8da164df
aidanjungo <aidan@cfse.ch>
2022-03-15 16:32:08 +0100

    Improve conditions

--- gmshairfoil2d/run_gmsh_airfoil_2d.py
+++ gmshairfoil2d/run_gmsh_airfoil_2d.py
@@ -1,13 +1,16 @@
 import argparse
+import math
+import sys
+import os
+
+import gmsh
+
 from airfoil_func import (
     NACA_4_digit_geom,
     get_airfoil_points,
     get_all_available_airfoil_names,
 )
-from geometry_def import PlaneSurface, Circle, Rectangle, AirfoilSpline
-import gmsh
-import sys
-import math
+from geometry_def import AirfoilSpline, Circle, PlaneSurface, Rectangle
 
 # Instantiate the parser
 parser = argparse.ArgumentParser(description="Optional arguement description")
@@ -19,9 +22,8 @@ parser.add_argument(
     help="Display all airfoil available in the database : https://m-selig.ae.illinois.edu/ads/coord_database.html",
 )
 
-
 parser.add_argument(
-    "--naca", type=str, nargs="?", help="NACA airfoil 4 digit, ex : 4412"
+    "--naca", type=str, nargs="?", help="NACA airfoil 4 digit (default 0012)"
 )
 
 parser.add_argument(
@@ -37,7 +39,7 @@ parser.add_argument(
     "--farfield",
     type=float,
     nargs="?",
-    help="Create a circular farfield mesh of given radius [m]",
+    help="Create a circular farfield mesh of given radius [m] (default 10m)",
 )
 parser.add_argument(
     "--box",
@@ -49,109 +51,117 @@ parser.add_argument(
     "--foil_mesh_size",
     type=float,
     nargs="?",
-    help="Mesh size of the airfoil countour [m]",
+    help="Mesh size of the airfoil countour [m]  (default 0.01m)",
 )
 
 parser.add_argument(
     "--ext_mesh_size",
     type=float,
     nargs="?",
-    help="Mesh size of the external domain [m]",
+    help="Mesh size of the external domain [m] (default 0.2m)",
 )
 
 parser.add_argument(
     "--format",
     type=str,
     nargs="?",
-    help="format of the mesh file ex: .su2",
+    help="format of the mesh file, e.g: msh, vtk, wrl, stl, mesh, cgns, su2, dat (default su2)",
 )
 
 parser.add_argument(
-    "--o",
+    "--output",
     type=str,
     nargs="?",
-    help="output path for the mesh file ex : home/myfolde/",
+    help="output path for the mesh file ex : /home/mymeshes (default : current dir)",
 )
 
 # Switch GUI
-parser.add_argument("--ui", action="store_true", help="Open GMSH GUI to see the mesh")
+parser.add_argument(
+    "--ui", action="store_true", help="Open GMSH user interface to see the mesh"
+)
 args = parser.parse_args()
 
 if len(sys.argv) == 1:
     parser.print_help()
     sys.exit()
 
-if args.list is True:
+if args.list:
     get_all_available_airfoil_names()
+    sys.exit()
 
+
+# Airfoil choice
+
+cloud_points = None
+if args.naca:
+    airfoil_name = args.naca
+    cloud_points = NACA_4_digit_geom(airfoil_name)
+
+if args.airfoil:
+    airfoil_name = args.airfoil
+    cloud_points = get_airfoil_points(airfoil_name)
+
+if cloud_points is None:
+    print("\nNo airfoil profile specified, exiting")
+    print("You must use --naca or --airfoil\n")
+    parser.print_help()
+    sys.exit()
+
+# Angle of attack
+aoa = 0
+if args.aoa:
+    aoa = -args.aoa * (math.pi / 180)
+
+# Generate Geometry :
+gmsh.initialize()
+
+# 1) External domain
+ext_mesh_size = 0.2
+if args.ext_mesh_size:
+    ext_mesh_size = args.ext_mesh_size
+
+if args.box:
+    lenght, width = [float(value) for value in args.box.split("x")]
+    ext_domain = Rectangle(0.5, 0, 0, lenght, width, mesh_size=ext_mesh_size)
 else:
-    # Foil and angle of attack
-    if args.naca is None:
-        cloud_points = get_airfoil_points(args.airfoil)
-    else:
-        # Generate NACA 4 digit profil
-        print(args)
-        cloud_points = NACA_4_digit_geom(args.naca)
+    radius = 10
+    if args.farfield:
+        radius = args.farfield
 
-    if args.aoa is None:
-        aoa = 0
-    else:
-        aoa = -args.aoa * (math.pi / 180)
+    ext_domain = Circle(0.5, 0, 0, radius=radius, mesh_size=ext_mesh_size)
 
-    # Generate Geometry :
-    gmsh.initialize()
+# 2) Airfoil
+mesh_size_foil = 0.01
+if args.foil_mesh_size:
+    mesh_size_foil = args.foil_mesh_size
 
-    # 1)External domain
+airfoil = AirfoilSpline(cloud_points, mesh_size_foil)
+airfoil.rotation(aoa, (0.5, 0, 0), (0, 0, 1))
+airfoil.gen_skin()
 
-    if args.ext_mesh_size is None:
-        ext_mesh_size = 0.2
-    else:
-        ext_mesh_size = args.ext_mesh_size
+# 3)Generate domain
+surface_domain = PlaneSurface([ext_domain, airfoil])
 
-    if args.farfield is not None:
-        # create a circular farfield
-        ext_domain = Circle(0.5, 0, 0, radius=args.farfield, mesh_size=ext_mesh_size)
-    elif args.box is not None:
-        # create a Box (lenght x height)
-        lenght, width = [float(value) for value in args.box.split("x")]
-        ext_domain = Rectangle(0.5, 0, 0, lenght, width, mesh_size=ext_mesh_size)
-    else:
-        # if no argument is given create a circular farfield by default
-        ext_domain = Circle(0.5, 0, 0, radius=10, mesh_size=0.2)
+# Synchronize and generate BC marker
+gmsh.model.occ.synchronize()
+ext_domain.define_bc()
+airfoil.define_bc()
+surface_domain.define_bc()
 
-    if args.foil_mesh_size is None:
-        mesh_size_foil = 0.01
-    else:
-        mesh_size_foil = args.foil_mesh_size
-    # 2)Airfoil
-    airfoil = AirfoilSpline(cloud_points, mesh_size_foil)
-    airfoil.rotation(aoa, (0.5, 0, 0), (0, 0, 1))
-    airfoil.gen_skin()
+# Generate mesh
+gmsh.model.mesh.generate(2)
+if args.ui:
+    gmsh.fltk.run()
 
-    # 3)Generate domain
-    surface_domain = PlaneSurface([ext_domain, airfoil])
+# Mesh file name and output
+format = "su2"
+if args.format:
+    format = args.format
 
-    # Synchronize and generate BC marker
-    gmsh.model.occ.synchronize()
-    ext_domain.define_bc()
-    airfoil.define_bc()
-    surface_domain.define_bc()
+path_file = ""
+if args.output:
+    path_file = args.o
 
-    # Generate mesh
-    gmsh.model.mesh.generate(2)
-    if args.ui is True:
-        gmsh.fltk.run()
-
-    # mesh file name and output
-    if args.format is None:
-        format = ".su2"
-    else:
-        format = args.format
-
-    if args.o is None:
-        path_file = ""
-    else:
-        path_file = args.o
-
-    gmsh.write(path_file + "mesh" + format)
-    gmsh.finalize()
+mesh_path = os.path.join(path_file, f"mesh_airfoil_{airfoil_name}.{format}")
+gmsh.write(mesh_path)
+gmsh.finalize()
