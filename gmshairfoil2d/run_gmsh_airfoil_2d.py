33c7299481146c6a35e3b44cec21fefad3608e5c
aidanjungo <aidan@cfse.ch>
2022-03-16 11:22:28 +0100

    Improve formating and default value

--- gmshairfoil2d/run_gmsh_airfoil_2d.py
+++ gmshairfoil2d/run_gmsh_airfoil_2d.py
@@ -13,9 +13,14 @@ from airfoil_func import (
 from geometry_def import AirfoilSpline, Circle, PlaneSurface, Rectangle
 
 # Instantiate the parser
-parser = argparse.ArgumentParser(description="Optional arguement description")
+parser = argparse.ArgumentParser(
+    description="Optional argument description",
+    usage=argparse.SUPPRESS,
+    formatter_class=lambda prog: argparse.HelpFormatter(
+        prog, max_help_position=80, width=99
+    ),
+)
 
-# Switch : Display list of available airfoil
 parser.add_argument(
     "--list",
     action="store_true",
@@ -23,41 +28,59 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    "--naca", type=str, nargs="?", help="NACA airfoil 4 digit (default 0012)"
+    "--naca",
+    type=str,
+    metavar="4DIGITS",
+    nargs="?",
+    help="NACA airfoil 4 digit (default 0012)",
 )
 
 parser.add_argument(
     "--airfoil",
     type=str,
+    metavar="NAME",
     nargs="?",
     help="Name of an airfoil profile in the database (database available with the --list argument)",
 )
 
-parser.add_argument("--aoa", type=float, nargs="?", help="Angle of attack in deg")
+parser.add_argument(
+    "--aoa",
+    type=float,
+    nargs="?",
+    help="Angle of attack [deg] (default: 0 [deg])",
+    default=0.0,
+)
 
 parser.add_argument(
     "--farfield",
     type=float,
+    metavar="RADIUS",
     nargs="?",
+    default=10,
     help="Create a circular farfield mesh of given radius [m] (default 10m)",
 )
 parser.add_argument(
     "--box",
     type=str,
+    metavar="LENGTHxWIDTH",
     nargs="?",
-    help="Create a box mesh of dimensions [lenght]x[height] [m]",
+    help="Create a box mesh of dimensions [length]x[height] [m]",
 )
 parser.add_argument(
-    "--foil_mesh_size",
+    "--airfoil_mesh_size",
     type=float,
+    metavar="SIZE",
     nargs="?",
+    default=0.01,
     help="Mesh size of the airfoil countour [m]  (default 0.01m)",
 )
 
 parser.add_argument(
     "--ext_mesh_size",
     type=float,
+    metavar="SIZE",
     nargs="?",
+    default=0.2,
     help="Mesh size of the external domain [m] (default 0.2m)",
 )
 
@@ -65,20 +88,23 @@ parser.add_argument(
     "--format",
     type=str,
     nargs="?",
+    default="su2",
     help="format of the mesh file, e.g: msh, vtk, wrl, stl, mesh, cgns, su2, dat (default su2)",
 )
 
 parser.add_argument(
     "--output",
     type=str,
+    metavar="PATH",
     nargs="?",
-    help="output path for the mesh file ex : /home/mymeshes (default : current dir)",
+    default=".",
+    help="output path for the mesh file (default : current dir)",
 )
 
-# Switch GUI
 parser.add_argument(
     "--ui", action="store_true", help="Open GMSH user interface to see the mesh"
 )
+
 args = parser.parse_args()
 
 if len(sys.argv) == 1:
@@ -89,9 +115,7 @@ if args.list:
     get_all_available_airfoil_names()
     sys.exit()
 
-
 # Airfoil choice
-
 cloud_points = None
 if args.naca:
     airfoil_name = args.naca
@@ -108,38 +132,24 @@ if cloud_points is None:
     sys.exit()
 
 # Angle of attack
-aoa = 0
-if args.aoa:
-    aoa = -args.aoa * (math.pi / 180)
+aoa = -args.aoa * (math.pi / 180)
 
-# Generate Geometry :
+# Generate Geometry
 gmsh.initialize()
 
-# 1) External domain
-ext_mesh_size = 0.2
-if args.ext_mesh_size:
-    ext_mesh_size = args.ext_mesh_size
-
+# External domain
 if args.box:
-    lenght, width = [float(value) for value in args.box.split("x")]
-    ext_domain = Rectangle(0.5, 0, 0, lenght, width, mesh_size=ext_mesh_size)
+    length, width = [float(value) for value in args.box.split("x")]
+    ext_domain = Rectangle(0.5, 0, 0, length, width, mesh_size=args.ext_mesh_size)
 else:
-    radius = 10
-    if args.farfield:
-        radius = args.farfield
+    ext_domain = Circle(0.5, 0, 0, radius=args.farfield, mesh_size=args.ext_mesh_size)
 
-    ext_domain = Circle(0.5, 0, 0, radius=radius, mesh_size=ext_mesh_size)
-
-# 2) Airfoil
-mesh_size_foil = 0.01
-if args.foil_mesh_size:
-    mesh_size_foil = args.foil_mesh_size
-
-airfoil = AirfoilSpline(cloud_points, mesh_size_foil)
+# Airfoil
+airfoil = AirfoilSpline(cloud_points, args.airfoil_mesh_size)
 airfoil.rotation(aoa, (0.5, 0, 0), (0, 0, 1))
 airfoil.gen_skin()
 
-# 3)Generate domain
+# Generate domain
 surface_domain = PlaneSurface([ext_domain, airfoil])
 
 # Synchronize and generate BC marker
@@ -150,18 +160,12 @@ surface_domain.define_bc()
 
 # Generate mesh
 gmsh.model.mesh.generate(2)
+
+# Open user interface of GMSH
 if args.ui:
     gmsh.fltk.run()
 
 # Mesh file name and output
-format = "su2"
-if args.format:
-    format = args.format
-
-path_file = ""
-if args.output:
-    path_file = args.o
-
-mesh_path = os.path.join(path_file, f"mesh_airfoil_{airfoil_name}.{format}")
+mesh_path = os.path.join(args.output, f"mesh_airfoil_{airfoil_name}.{args.format}")
 gmsh.write(mesh_path)
 gmsh.finalize()
